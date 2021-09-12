5dd376bf6db9310b8db392d094717a4ee92a4db1
hrachishkhanyan <hrachishkhanyan@gmail.com>
2021-09-12 13:13:08 +0400

    Translated

--- Homeworks/day_4_homework.py
+++ Homeworks/day_4_homework.py
@@ -1,12 +1,16 @@
 """LOOPS"""
 
 # 1. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a nested loop.
+# Օգտագործելով ցիկլեր, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
 
 # 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
+# Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
 
 
 # 4. Count the number of 'b's in the given string. DO NOT use the count() method.
+# Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
 nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
 
 # 5. Write a program that will print the factorial of n.
+# Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
 
