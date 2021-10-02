b03869303fb0d6698721581a9e5b833489b9869d
hrachishkhanyan <hrachishkhanyan@gmail.com>
2021-10-02 14:16:22 +0400

    Day 7 hw translated

--- Homeworks/day_7_homework.py
+++ Homeworks/day_7_homework.py
@@ -2,12 +2,42 @@
 
 # 1. Write a recursive function to calculate the sum of reciprocals of powers of 2. Try increasing the n and see the
 # magic.
+# Գրել ռեկուրսիվ ֆունկցիա, որը կհաշվի երկրաչափական գումարը, որտեղ r = 1 / 2 է։ Փորձեք բացրձրացնել n-ի արժեքը հետաքրքիր
+# արդյունք ստանալու համար։
 
 # 2. Write a recursive function to calculate n-th power of a.
+# Գրել ֆունկցիա, որը ռեկուրսիվ կերպով կհաշվի a-ի n աստիճանը։
 
 # 3. Write a recursive function that evaluates a mathematical expression. Example input - "5 + 6"
+# Գրել ռեկուրսիվ ֆունկցիա, որը կհաշվի մաթեմատիկական արտահայտություն փոխանցված սթրինգի միջոցով։ Օրինակ՝ "5 + 6"
 
 # 4. Write a recursive function that reverses a string
+# Գրել ֆունկցիա, որը ռեկուրսիվ կերպով կշրջի և կվերադարձնի փոխանցված սթրինգը։
 
 # 5. Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved
 # to the end of the string.
+# Գրել ռեկուրսիվ ֆունկցիա, որը որպես արգումենտ կվերցնի սթրինգ և կվերադարձնի նոր սթրինգ, որտեղ օրիգինալ սթրինգում գտնվող
+# բոլոր x-երը տեղափոխվել են սթրինգի ամենավերջը։
+
+"""OLD STUFF"""
+
+# 6. Write a function that sorts a list in ascending. Additionally, the function may take a keyword argument that will
+# reverse the sort (without recursion is fine).
+# Գրել ֆունկցիա, որը կսորտավորի դրան փոխանցված լիստը աճման կարգով։ Կարելի է ֆունկցիային նաև ավելացնել kwarg, որը փոփոխելիս
+# կկարողանանք լիստը սորտավորել նաև նվազման կարգով։
+
+# 7. Write a function that will take 4 arguments. First two are tuples and represent 2D coordinates of two circles.
+# The others are the radii of our circles. The function must tell us whether one of the circles is inside the other, or
+# do circles intersect, or are they far apart.
+# Գրել ֆունկցիա, որը կվերցնի 4 արգումենտ։ Առաջին երկուսը tuple են, որոնք իրենցից ներկայացնելու են 2-չափ տարածության մեջ
+# շրջանագծերի կենտրոնների կոորդինատներ։ Մյուս երկուսը լինելու են integer տիպի և ներկայացնելու են վերոնշյալ շրջանագծերի
+# շառավիղները։ Ֆունկցիան պետք է տպի արդյո՞ք օղակները հատվում են, կամ մեկը մյուսի մեջ է, կամ իրարից հեռու են։
+
+"""OPTIONAL"""
+
+# 8. Write a program to combine two dictionaries adding values for common keys.
+# Գրել ծրագիր, որը կմիացնի երկու dictionary։ Համընկնող բանալիների արժեքները պետք է գումարվեն։
+
+# 9. Write a program to create a dictionary from a string. The letters are the keys and the values are the counts of
+# the corresponding letters.
+# Սթրինգից ստեղծել dictionary: Սթրինգի տառերը հանդես են գալու որպես բանալիներ, իսկ սթրինգում դրանց քանակը որպես արժեքներ
